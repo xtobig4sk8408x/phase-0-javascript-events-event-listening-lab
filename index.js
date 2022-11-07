f2a51047ac3d0ef854732e8a14d45fde0cfcab69
Theodore John Josef <john.josef@aol.com>
2022-11-07 09:44:41 -0800

    commit tree f1939fb2c709cd02294e057caffc4c45e4589324

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let input;
+
 function addingEventListener() {
+    input = document.getElementById("button");
+    input.addEventListener('click', () => {
+        console.log(input);
+    })
 }
+
+addingEventListener();
